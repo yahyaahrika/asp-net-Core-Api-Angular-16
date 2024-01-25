b5df54feecc3c02bbdebe2cfd24257320cdbfe1c
yahya <yahyatechdata@gmail.com>
2024-01-25 22:58:38 +0100

    file environment Config ApiUrl

--- Anime_FrontEnd/src/app/shared.service.ts
+++ Anime_FrontEnd/src/app/shared.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from './../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +9,7 @@ export class SharedService {
  
 
   constructor(private http:HttpClient) {   }
-  private url = "//localhost:5113/api/Animes"
+  private url = environment.ApiUrl;
 
   animes:any[]= [];
 
